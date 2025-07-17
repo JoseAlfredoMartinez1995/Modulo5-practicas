c82c5344edfb46d41af0e4ba87fa78fac166b81c
JoseAlfredoMartinez1995 <alfredo12131995@gmail.com>
2025-07-16 22:52:21 -0600

    Primer commit

---
+++ Tema02/src/index.js
@@ -0,0 +1,31 @@
+import _ from 'lodash';
+import './estilo.css';
+import imagen from './astronauta.png';
+import datos from './datos.csv';
+
+import ayaml from './datosy.yaml';
+import ajson5 from './datosj.json5';
+
+function Componente()
+{
+    const elemento = document.createElement('div');
+
+    //lodash
+    elemento.innerHTML = _.join(['HOLA un webpack!', 'Webpack'], ' ');
+    //elemento.innerHTML = 'Hola' + 'Webpack normal';
+    elemento.classList.add('hola');
+    
+    const miImagen = new Image();
+    miImagen.src = imagen;
+    elemento.appendChild(miImagen);
+
+    console.log(datos);
+
+    console.log(ayaml.company);
+
+    console.log(ajson5.nombre);
+
+    return elemento;
+}
+
+document.body.appendChild(Componente());
