9fe2b57947351d88eccdbfc4fcb9595bc8a0d902
JoseAlfredoMartinez1995 <alfredo12131995@gmail.com>
2025-07-25 00:14:47 -0600

    Quinto commit

---
+++ Tema04/src/index.js
@@ -0,0 +1,42 @@
+import _ from 'lodash';
+import './estilo.css';
+import imagen from './astronauta.png';
+import datos from './datos.csv';
+
+import ayaml from './datosy.yaml';
+import ajson5 from './datosj.json5';
+
+if('serviceWorker' in navigator)
+{
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('./service-worker.js').then(registration => {
+            console.log("SW registrado", registration);
+        }).catch(err => {
+            console.log("SW no registrado", err)
+        });
+    });
+}
+
+function Componente()
+{
+    const elemento = document.createElement('div');
+
+    //lodash
+    elemento.innerHTML = _.join(['HOLA', 'Webpack'], ' ');
+    elemento.classList.add('hola');
+    
+    const miImagen = new Image();
+    miImagen.src = imagen;
+    elemento.appendChild(miImagen);
+
+    console.log(datos);
+
+    console.log(ayaml.company);
+
+    console.log(ajson5.nombre);
+
+    return elemento;
+}
+
+document.body.appendChild(Componente());
+document.body.classList.add('fondo');
